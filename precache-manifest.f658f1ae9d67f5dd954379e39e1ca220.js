31a724446dc2d1fab7ea608cfcb4e9361beeecad
simone-paglino <simpag99@gmail.com>
2021-03-01 17:46:20 +0000

    Deploying to gh-pages from @ simone-paglino/reusable-components@57c2374d81221e0596044c4de4d3e25d66770124 🚀

---
+++ precache-manifest.f658f1ae9d67f5dd954379e39e1ca220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8852afeb4b6dd086b28753c019a4412f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d5fc0ba33dfba8c03df",
+    "url": "/static/css/main.a5278118.chunk.css"
+  },
+  {
+    "revision": "ccb8083fd23807d473c5",
+    "url": "/static/js/2.d4f73b97.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/static/js/2.d4f73b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5fc0ba33dfba8c03df",
+    "url": "/static/js/main.a715cf05.chunk.js"
+  },
+  {
+    "revision": "1aa768f1845c66a5394b",
+    "url": "/static/js/runtime-main.bcae4b98.js"
+  },
+  {
+    "revision": "4c780950be8fb12ec36c737deacdfdb7",
+    "url": "/static/media/arrow_down.4c780950.svg"
+  }
+]);
